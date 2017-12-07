0de38bf1948cb3470e7ca9a3caf0ace308c7e609
flambok <amine@flambok.com>
2017-12-07 09:49:56 +0100

    Added About

---
+++ components/About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {} from 'react-native'
+
+export default class About extends Component {
+    render() {
+        return (
+            <View>
+                <Text>A propos de Foohachi</Text>
+                <Text>
+                        Lorem trium sententiarum nulli prorsus assentior.
+                        Haec subinde Constantius audiens et quaedam
+                        Restabat ut Caesar post haec properaret accitus et
+                        Mensarum enim voragines et varias voluptatum
+                        Sed (saepe enim redeo ad Scipionem, cuius omnis
+                </Text>
+            </View>
+        );
+    }
+}
